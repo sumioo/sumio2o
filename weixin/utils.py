1ea220ad2d0adfa125bcc438270bd1abd7ed66c8
sumioo <jiya81@qq.com>
2015-07-09 01:23:30 +0800

    file
    add after

---
+++ weixin/utils.py
@@ -0,0 +1,132 @@
+#coding:utf-8
+
+from hashlib import sha1
+import xml.etree.ElementTree as ET
+import urllib2,urllib
+from HTMLParser import HTMLParser
+import re
+
+from sae.taskqueue import Task, TaskQueue
+
+
+TOKEN='sumioo'
+
+def echo_str(query_dict):
+    signature=query_dict.get('signature','')
+    timestamp=query_dict.get('timestamp','')
+    nonce=query_dict.get('nonce','')
+    echostr=query_dict.get('echostr','')
+    l=[TOKEN,timestamp,nonce]
+    l.sort()
+    st=''.join(l)
+    st=sha1(st).hexdigest()
+    if st == signature:
+        return echostr
+    else:
+        return False
+
+def verify_source(query_dict):
+    '''
+    verify the request's source.argument query_dict is 'GET' method argument
+    '''
+    signature=query_dict.get('signature','')
+    timestamp=query_dict.get('timestamp','')
+    nonce=query_dict.get('nonce','')
+    l=[TOKEN,timestamp,nonce]
+    l.sort()
+    st=''.join(l)
+    st=sha1(st).hexdigest()
+    if st == signature:
+        return True
+    else:
+        return False
+
+def convert_xml_to_dict(request):
+    '''
+    process the post xml data,return all tags and its text as a dict .
+    '''
+    xml_dict={}
+    xml_tree=ET.parse(request)
+    root=xml_tree.getroot()
+    for child in root:
+        xml_dict[child.tag]=child.text
+    return xml_dict
+
+
+def fetch_url(key_word,method,page):
+    k = urllib.urlencode({method:key_word.encode('gbk')})
+    url ='http://210.38.245.168:81/searchresult.aspx'
+    params='?%s&dt=ALL&cl=ALL&dept=ALL&sf=M_PUB_YEAR&ob=DESC&page=%s&dp=45&sm=table' %(k,page)
+    url=url+params
+    print url
+    headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 UBrowser/4.0.3647.11 Safari/537.36'}
+    req=urllib2.Request(url=url,headers=headers)
+    return urllib2.urlopen(req) #file-like object
+
+
+class Myhtmlparser(HTMLParser):
+    d=['searchresult_cb','title','author','press','year','ac_num','quantity','lendable']
+    pattern1=('id','ctl00_ContentPlaceHolder1_countlbl') # total numbers pattern
+    pattern2=('id','ctl00_ContentPlaceHolder1_dplblfl1') #current page pattern
+    pattern3=('id','ctl00_ContentPlaceHolder1_gplblfl1') #total page pattren
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.flag,self.flag2=False,False
+        self.nums=[]
+        self.temp=[]
+        self.rt=[]
+        self.data=[]
+    def handle_starttag(self,tag,attrs):
+        if tag=='tbody':
+            self.flag=True
+        elif tag == 'span' and (Myhtmlparser.pattern1 in attrs or Myhtmlparser.pattern2 in attrs or Myhtmlparser.pattern3 in attrs):
+            self.flag2=True
+    def handle_data(self,data):
+            if self.flag and (not data.isspace()):
+                self.data.append(data)
+            elif self.flag2:
+                self.nums.append(int(data))
+    def handle_endtag(self,tag):
+            if self.flag and tag=='td':
+                self.temp.append(''.join(self.data))
+                self.data=[]
+            if self.flag and tag=='tr':
+                #print self.temp
+                self.rt.append(dict(zip(Myhtmlparser.d,self.temp)))
+                self.temp=[]
+            if tag=='tbody':
+                    self.flag=False
+            elif tag=='span':
+                self.flag2=False
+
+
+def query_library(key_word,title,page):
+    '''
+    open the school library search page
+    return [ {},.....]
+    '''
+    html=fetch_url(key_word,title,page).read()
+    parser=Myhtmlparser()
+    parser.feed(html)
+    if parser.rt:
+        return parser.rt,parser.nums[0],parser.nums[1],parser.nums[2] #parser[0,1,2]:total nums,current page,total page;
+    else:
+        return [],0,0,0
+"""
+def set_kvdb(x_dict):
+    '''
+    set sae kvdb
+    '''
+    user_dict={'rt':rt,'next':9,'current_page':current_page,'total_page':total_page,'total_nums':total_nums,'key_word':content}
+    kv.set(from_username,user_dict)
+"""
+
+def add_task(user_dict,url,user):
+    '''
+    add sae queue task ,fetch next page
+    '''
+    url=url+'?&from=%s' %user
+    queue = TaskQueue('fetch_next')
+    queue.add(Task(url))
+    return None
+
---
+++ weixin/views.py
@@ -0,0 +1,100 @@
+#coding:utf-8
+from hashlib import sha1
+from time import time
+
+from django import template
+from django.http import HttpResponse,HttpResponseRedirect
+import sae.kvdb
+from utils import *
+
+kv=sae.kvdb.Client()
+
+def init_srever(request):
+    if request.method=='GET':
+        return HttpResponse(echo_str(request.GET))
+
+def index(request):
+    if request.method=='POST':
+        if verify_source(request.GET):
+            x_dict=convert_xml_to_dict(request)
+            from_username=x_dict['FromUserName']
+            content=x_dict['Content'].strip()
+            if content != '1':
+                kv.delete(from_username) #remove kv user_dict
+                rt,total_nums,current_page,total_page=query_library(content,'title','1')
+                sub_rt=rt[0:9]            #display 9 items on weixin
+                len_sub_rt=len(sub_rt)
+                if len_sub_rt == 9:
+                    if len(rt) > 9:
+                        user_dict={'rt':rt,
+                                   'next':9,
+                                   'current_page':current_page,
+                                   'total_page':total_page,
+                                   'total_nums':total_nums,
+                                   'key_word':content
+                                   }
+                        kv.set(from_username,user_dict)   #save
+                    article_count='10'
+                    title=u'查询关键字:%s 结果数:%s' %(content,total_nums)
+                elif 0<len_sub_rt<9:
+                    article_count=len_sub_rt+2
+                    title=u'查询关键字:%s 结果数:%s' %(content,total_nums)
+                else:
+                    article_count='1'
+                    title=u'查询关键字:%s 结果数:%s' %(content,total_nums)
+                    description=u'没有您要检索的馆藏书目！'
+
+            else:
+                user_dict=kv.get(from_username)
+                if user_dict:
+                    _next=user_dict['next']
+                    sub_rt=user_dict['rt'][_next:_next+9]
+                    user_dict['next']+=9
+                    kv.replace(from_username,user_dict)
+                    len_sub_rt=len(sub_rt)
+                    if len_sub_rt == 9:             #the number of response items about books is 45
+                        article_count='10'
+                        title=u'查询关键字:%s 结果数:%s' %(user_dict['key_word'],user_dict['total_nums'])
+                        if sub_rt[-1] == user_dict['rt'][-1]:    #when go to the end index
+                            if user_dict['current_page'] != user_dict['total_page']:
+                                add_task(user_dict,'/task/',from_username)    # add task to sae queue to fetch next page
+                            else:
+                                kv.delete(from_username)          #if go to the end page delete user_dict,
+                    else:
+                        article_count=len_sub_rt+2
+                        title=u'查询关键字:%s 结果数:%s' %(user_dict['key_word'],user_dict['total_nums'])
+                        sub_rt=user_dict['rt'][_next:]
+                        kv.delete(from_username) #remove kv user_dict
+                else:
+
+                    article_count='1'
+                    title=u'没有更多的数据或查询下一页已过期' #return 'no more data'or'query outdate'
+
+            create_time=str(int(time()))
+            t=template.loader.get_template('news_response.xml')
+            c=template.Context(locals())
+            xml=t.render(c)
+            return HttpResponse(xml,content_type='application/xml')
+        else:
+            return HttpResponse('False')
+    else:
+        return HttpResponse('False')
+
+
+def fetch_next(request):
+    if request.method=='GET':
+        from_username=request.GET.get('from','0').encode('ascii')
+        user_dict=kv.get(from_username)
+        rt,total_nums,current_page,total_page=query_library(user_dict['key_word'],'title',user_dict['current_page']+1)
+        user_dict.update({'rt':rt,'current_page':current_page,'next':0})
+        kv.replace(from_username,user_dict)
+        return HttpResponse('done')
+
+
+
+
+
+
+
+
+
